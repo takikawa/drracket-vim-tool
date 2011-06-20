5a019252e91fa73d17be08cb2d512b40d8ae1afe
Asumu Takikawa <asumu@ccs.neu.edu>
2011-06-20 15:48:06 -0400

    Use unit lang

--- tool.rkt
+++ tool.rkt
@@ -1,35 +1,33 @@
-#lang racket/gui
+#lang racket/unit
 
 (require drracket/tool
          framework
+         racket/class
+         racket/gui
          "private/text.rkt")
 
-(provide tool@)
+(import drracket:tool^)
+(export drracket:tool-exports^)
 
-(define-unit tool@
-  (import drracket:tool^)
-  (export drracket:tool-exports^)
-  
-  (define vim-frame-mixin
-    (mixin (drracket:unit:frame<%>) ()
-  
-      (inherit get-definitions-text)
-  
-      (define/override (edit-menu:between-find-and-preferences edit-menu)
-        (super edit-menu:between-find-and-preferences edit-menu)
-  
-        (new checkable-menu-item%
-             [label "Vim Mode"]
-             [parent edit-menu]
-             [callback 
-               (lambda (i e) (send (get-definitions-text) toggle-vim!))]
-             [checked (send (get-definitions-text) vim?)]))
-  
-      (super-new)))
+(define vim-frame-mixin
+  (mixin (drracket:unit:frame<%>) ()
 
-  (define (phase1) (void))
-  (define (phase2) (void))
+    (inherit get-definitions-text)
 
-  (preferences:set-default 'drracket:vim-emulation? #f (lambda (x) (boolean? x)))
-  (drracket:get/extend:extend-definitions-text vim-emulation-mixin)
-  (drracket:get/extend:extend-unit-frame vim-frame-mixin))
+    (define/override (edit-menu:between-find-and-preferences edit-menu)
+      (super edit-menu:between-find-and-preferences edit-menu)
+      (new checkable-menu-item%
+           [label "Vim Mode"]
+           [parent edit-menu]
+           [callback
+             (λ (i e) (send (get-definitions-text) toggle-vim!))]
+           [checked (send (get-definitions-text) vim?)]))
+
+    (super-new)))
+
+(define (phase1) (void))
+(define (phase2) (void))
+
+(preferences:set-default 'drracket:vim-emulation? #f (lambda (x) (boolean? x)))
+(drracket:get/extend:extend-definitions-text vim-emulation-mixin)
+(drracket:get/extend:extend-unit-frame vim-frame-mixin)
