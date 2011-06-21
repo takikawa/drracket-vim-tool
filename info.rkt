09bfdf0cf9dcf9ce99da85d4ecfc3bf680f11268
Asumu Takikawa <asumu@ccs.neu.edu>
2011-06-21 16:18:01 -0400

    Add PLaneT metadata

--- info.rkt
+++ info.rkt
@@ -3,3 +3,7 @@
 (define drracket-tools '(("tool.rkt")))
 (define drracket-tool-names (list "Vim Emulation"))
 (define drracket-tool-icons '(#f))
+
+(define blurb '("Vim-style editing for DrRacket"))
+(define categories '(devtools))
+(define primary-file "tool.rkt")
