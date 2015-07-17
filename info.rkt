658a02cc736978003cfdc3989b0f05c98713f30d
Vincent St-Amour <stamourv@racket-lang.org>
2015-07-17 11:49:22 -0500

    Remove dependency on unstable-list-lib.
    Instead get the relevant bindings from the core.

--- info.rkt
+++ info.rkt
@@ -8,7 +8,6 @@
 (define categories '(devtools))
 (define primary-file "tool.rkt")
 
-(define deps '("base" "gui-lib" "data-lib" "drracket-plugin-lib"
-               "unstable-list-lib"))
+(define deps '("base" "gui-lib" "data-lib" "drracket-plugin-lib"))
 (define single-collection "drracket-vim-tool")
 
