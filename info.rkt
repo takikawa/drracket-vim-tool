0d6725c85d732e7a6b92e3dc6f1f5d98b92ebd2c
Asumu Takikawa <asumu@ccs.neu.edu>
2015-09-06 23:02:29 -0400

    Re-add unstable dependency for this branch

--- info.rkt
+++ info.rkt
@@ -8,7 +8,8 @@
 (define categories '(devtools))
 (define primary-file "tool.rkt")
 
-(define deps '("base" "gui-lib" "data-lib" "drracket-plugin-lib"))
+(define deps '("base" "gui-lib" "data-lib" "drracket-plugin-lib"
+               "unstable-list-lib"))
 (define build-deps '("rackunit-lib" "at-exp-lib"))
 (define single-collection "drracket-vim-tool")
 
